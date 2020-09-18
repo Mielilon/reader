d44bc8545257670370101a4ac77db44422f75851
Alexander Gusev <san4es021@gmail.com>
2020-09-18 19:44:59 +0300

    Сделал модалки и икноки

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
 //= jquery.min.js
+
+
+$(".controls__icon").click(function (event) {
+  const modalName = $(this).parent()[0].getAttribute('data-open-modal')
+  console.log(modalName);
+  if(!modalName) return
+
+  const modalId = `#modal-${modalName}`
+  const isModalActive = $(modalId).hasClass('modal_active')
+
+  console.log(modalId);
+
+  $(`.modal:not(${modalId})`).removeClass('modal_active')
+  $(modalId).toggleClass('modal_active')
+
+  if(isModalActive) {
+    $(this).parent().children('.icon-active').addClass('hidden')
+    $(this).parent().children(':not(.icon-active)').removeClass('hidden')
+  } else {
+    $(this).parent().children('.icon-active').removeClass('hidden')
+    $(this).parent().children(':not(.icon-active)').addClass('hidden')
+  }
+})
