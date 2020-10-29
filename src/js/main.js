1d08063ca0a8e5eca1fc64b185482b91854f6652
Mielilon <daria.mg354@mail.ru>
2020-10-29 12:53:49 +0300

    Тест

--- src/js/main.js
+++ src/js/main.js
@@ -156,6 +156,9 @@ $('.format__item_fullscreen').click(function() {
 
 $('.reader-controls__full-screen').click(function() {
   toggleFullScreen()
+  if(preg_match('/iPhone|iPad|iPod/i', $_SERVER ['HTTP_USER_AGENT'])) {
+    $('body').toggleClass('body_fullscreen')
+  }
 }) 
 
 
